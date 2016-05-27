82e31e38f8ad8125ececff10c4aff74c0c3bfd7f
Alexis <alexismasseron@gmail.com>
2016-05-27 10:16:38 +0000

    updated version 0.2.0

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Uses noble for BLE peripheral scanning, then attempts to parse discovered periph
 Based on sandeepmistry project : https://github.com/sandeepmistry/node-eddystone-beacon-scanner
 
 Modified to scan and get all the sensors informations with a Sensoro Yunzi Beacon and store them in a JSON file.
+
+The JSON is then send to a remote server using websocket.
---
+++ scan.js
@@ -0,0 +1,16 @@
+var EddystoneYunziScanner = require('../'); // use require('eddystone-yunzi-scanner-), if installed from npm
+
+EddystoneYunziScanner.on('found', function(beacon){
+	console.log('Frame content:\n', JSON.stringify(beacon, null, 2));
+});
+
+EddystoneYunziScanner.on('updated', function(beacon) {
+	console.log('Frame content:\n', JSON.stringify(beacon, null, 2));
+});
+
+EddystoneYunziScanner.on('lost', function(beacon) {
+	console.log('Lost Yunzi beacon\n Last frame content:\n', JSON.stringify(beacon, null, 2));
+});
+
+EddystoneYunziScanner.startScanning(true); 
+
